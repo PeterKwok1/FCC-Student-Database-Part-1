8c123d0038738f1b00fa0db6acf0d90803fd3bcb
Peter Kwok <peter.kwok94@gmail.com>
2024-01-01 19:13:24 -0500

    Save progress: 800.1

--- insert_data.sh
+++ insert_data.sh
@@ -14,7 +14,6 @@ do
     then
         # insert major
         INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
-        echo $INSERT_MAJOR_RESULT
         # get new major_id
     fi
     # get course_id
