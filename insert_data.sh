e2b1ee1089440c16f93c09926b32709d8b5687c7
Peter Kwok <peter.kwok94@gmail.com>
2024-01-03 00:44:30 -0500

    Save progress: 1060.1

--- insert_data.sh
+++ insert_data.sh
@@ -59,5 +59,6 @@ do
     fi
     # set to null
     # insert student 
+    INSERT_STUDENT_RESULT=$($PSQL "INSERT INTO students(first_name, last_name, major_id, gpa) VALUES('$FIRST', '$LAST', $MAJOR_ID, $GPA)")
     fi
 done
